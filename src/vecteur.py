479a4aefde59f01ac54d999efe7a49fdfd57dc88
TheoSchlaeppi <schlaepitheo@outlook.com>
2021-04-05 18:29:18 +0200

    Premier test pour les vecteurs ( il n'y a pas encore un produit entre deux vecteurs

--- src/vecteur.py
+++ src/vecteur.py
@@ -1,7 +1,25 @@
 class Vecteur:
-    def __init__(self, x, y):
+    def __init__(self, x = 0, y = 0, r = 0, s = 0 ):
         self.x = x
         self.y = y
-if __name__=="__main__":
-    f = Vecteur(5, 3)
-    print(f)
+        self.r = r
+        self.s = s
+
+    def addvect(x = 0, y = 0, r = 0, s = 0):
+        a = x + r
+        b = y + s
+        return ( a, b)
+    def multivect(x = 0, y = 0, r = 0):
+        c = x * r  #seulement pour multiplier un variable a un vecteur
+        d = y * r
+        return ( c, d)
+
+k,l = Vecteur.addvect(2,1,-3,1)
+
+print(k,l)
+g, f = Vecteur.multivect(4,3,2)
+print(g, f)
+h = Vecteur.addvect(k,l,g,f)
+
+print(h)
+
