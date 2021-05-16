48b9a10105c2642ddbbb25709b717458c8a4bfd3
corentinsteinhauser <costein972@gmail.com>
2021-05-16 15:50:26 +0200

    correction de bugs sur graph.py

--- src/graph.py
+++ src/graph.py
@@ -1,31 +1,33 @@
 from water_rocket.src.rocket import Rocket
 from water_rocket.src.vecteur import Vecteur
-from numpy import cos, sin, tan, radians
+from math import cos, sin, tan, radians, degrees
 import matplotlib.pyplot as plt
 import logging
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
-def Cos(a):
-    return cos(radians(a))
-def Sin(a):
-    return sin(radians(a))
-def Tan(a):
-    return tan(radians(a))
+
 x0 = Vecteur(0, 0)
 
 x0.y = float(input("Hauteur de la fusée:"))
-if x0.y not in range(0,150):
+if x0.y > 150:
     logging.warning("La valeur de la hauteur de la fusée ne corresponds pas")
     raise NotImplementedError
+
 v_pas0 = float(input("vitesse de la fusée:"))
 
-teta = int(input("Angle de la fusée:"))
-if teta not in range(1,89):
+theta = float(input("Angle de la fusée:"))
+if theta > 360:
     logging.warning("L'angle de la fusée est trop élever")
     raise NotImplementedError
+theta_rad = radians(theta)
+print(theta_rad)
 
-v0 = Vecteur(Cos(teta)*v_pas0, Sin(teta)*v_pas0)
+v0 = Vecteur(cos(theta_rad)*v_pas0, sin(theta_rad)*v_pas0)
+
+if theta == 90 or theta == 270:
+    v0 = Vecteur(0, sin(theta_rad)*v_pas0)
+
 rocket = Rocket(x0, v0)
 time = 0
 
