4d343195eb1dace8215129d2e69ec148c25b1dde
corentinsteinhauser <costein972@gmail.com>
2021-06-13 16:21:51 +0200

    version 2 test class mover
    foctionne pour MRUA et energie mais pas pour pasApas et il manque la fontion pour choisire les forces

--- src/rocket.py
+++ src/rocket.py
@@ -11,40 +11,48 @@ class Rocket:
 
 
 class Mover(Rocket):
-    pass
+    def __init__(self, t):
+        self.t = t
+        super().__init__(self)
 
 class Forces(Mover):
     def Poids(self):
-        self.Poids = Vecteur(0, Mover.m * -9.81)
+        self.Poids = Vecteur(0, self.m * -9.81)
         return self.Poids
 
     def Force_frot(self):
-        self.Force_frot = Mover.v0 * Mover.v0
+        self.Force_frot = self.v0 * self.v0
 
     def somme_f(self):
-        return self.Poids + self.Force_frot
+        return Vecteur(self.Poids + self.Force_frot)
 
 
 class Energie(Mover):
     def energie(self):
-        self.energie = 0.5 * Mover.m * Mover.v0 * Mover.v0 + Mover.m * Mover.a0 * Mover.x0
+        self.energie = 0.5 * self.m * self.v0 * self.v0 + self.m * self.a0 * self.x0
         return self.energie
 
 
 class MRUA(Mover):
     def Mrua(self):
-        Mover.x0 = Mover.x0 + Mover.v0 * Mover.t + 0.5 * Mover.a0 * Mover.t * Mover.t
-        return Mover.x0
+        self.x0 = self.x0 + self.v0 * self.t + 0.5 * self.a0 * self.t * self.t
+        return self.x0
 
 
 class PasAPas(Mover):
     def pas_a_pas(self):
-        Mover.a0 = Vecteur(Forces.somme_f / Mover.m)
-        Mover.v0 = Mover.a0 * Mover.t + Mover.v0
-        Mover.x0 += Mover.v0 * Mover.t
-        return Mover.x0, Mover.v0
-if __name__ == "__main__": #pour tester la class
-    gten = Rocket(12, 5, 65)
-    gten = Energie.energie(gten)
+        self.a0 = Vecteur(Forces.somme_f / Vecteur(self.m))
+        self.v0 = self.a0 * self.t + self.v0
+        self.x0 += self.v0 * self.t
+        return self.x0, self.v0
 
+if __name__ == "__main__": #pour tester la class
+    gten = Rocket(10, 1, 23)
+    gten = Energie.energie(gten)
+    print(gten)
+
+    fff = Rocket(2, 1, 3)
+    fff.t = 1
+    fdd = MRUA.Mrua(fff)
+    print(fdd)
 
