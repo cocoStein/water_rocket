b9e4de1835785ebc8c41c1f4f7a5e1867cfefb1e
corentinsteinhauser <costein972@gmail.com>
2021-06-29 11:03:51 +0200

    classe move
    plus de return mais des overright sur les self

--- src/rocket.py
+++ src/rocket.py
@@ -1,5 +1,5 @@
 from water_rocket.src.vecteur import Vecteur
-
+from math import *
 
 class Rocket:
     #Definition de la fusée avec toutes les variables nécessaire au programme
@@ -17,61 +17,27 @@ class Rocket:
 
     def energie(self):
         # type de mouvement avec l'énergie cinétique et potentielle
-        return 0.5 * self.m * self.v0 * self.v0 + self.m * self.a0 * self.x0
-
-class Mover(Rocket):
-    #selecteur entre les différents types de mover
-    def __init__(self, t):
-        self.t = t
-        super().__init__(self)
-
-#class Forces(Rocket):
-#    #class des forces ou l'on peut rajouter et choisir les forces que l'on souhaite prendre en compte pour la somme des forces
-#    def __init__(self, P = 1, Ff = 1):
-#        self.P = P
-#        self.Ff = Ff
-#    P = 1       #pas la bonne solution pour le faire mais si je le fais avec la classe alors tous est décalé et je n'arriv plus à définir les def
-#    Ff = 1
-#    if P == 1:
-#        def Poids(self):
-#        #poids de l'objet
-#            self.Poids = Vecteur(0, self.m * -9.81)
-#            return self.Poids
-#
-#    elif Ff == 1:
-#        def Force_frot(self):
-#            #force de frottement de l'objet
-#            self.Force_frot = self.v0 * self.v0
-#            return self.Force_frot
-#    else:
-#        pass
-#
-#    def somme_f(self):
-#        # somme des forces prisent en compte
-#            return Vecteur(Forces.Poids() + Forces.Force_frot() #je ne comprends pas pk Forces n'a pas de Poids et de ff ?
-
-
-#class Energie(Mover):
-#    def energie(self):
-#        # type de mouvement avec l'énergie cinétique et potentielle
-#      return 0.5 * self.m * self.v0 * self.v0 + self.m * self.a0 * self.x0
+        self.v0 = sqrt(2*self.a0*self.x0)
+        self.x0 = (self.v0*self.v0)/(2*self.a0)
+         #0.5 * self.m * self.v0 * self.v0 + self.m * self.a0 * self.x0
 
 
 
-class MRUA(Mover):
-    def mrua(self):
+class MRUA():
+    def move(self, Rocket, dt):
         # type de mouvenemt avec des MRUA basiques
-        return self.x0 + self.v0 * self.t + 0.5 * self.a0 * self.t * self.t
+        Rocket.v0 += Rocket.a0*dt
+        Rocket.x0 += Rocket.v0 * dt + 0.5 * Rocket.a0 * dt * dt
+         #Rocket.x0 + Rocket.v0 * dt + 0.5 * Rocket.a0 * dt * dt
 
 
-class PasAPas(Mover):
-    def pas_a_pas(self):
+class PasAPas():
+    def move(self, Rocket, dt):
         #type de mouvement dit Pas à Pas (version 1)
         forces = Rocket.somme_f(self)
-        self.a0 = Vecteur(forces.x / self.m, forces.y / self.m)
-        self.v0 = self.a0 * self.t + self.v0
-        self.x0 += self.v0 * self.t
-        return self.x0
+        Rocket.a0 = Vecteur(forces.x / Rocket.m, forces.y / Rocket.m)
+        Rocket.v0 = Rocket.a0 * dt + Rocket.v0
+        Rocket.x0 += Rocket.v0 * dt
 
 if __name__ == "__main__": #pour tester la class
     v0 = Vecteur(12,78)
